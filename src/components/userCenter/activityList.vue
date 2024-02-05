5a7bcab0dbd12338cfc26cd612b4707a62038625
Sandy <sandy.chiu@lyls.com.tw>
2024-02-05 13:18:27 +0800

    新增會員中心頁面、sidebar、各分頁的component

---
+++ src/components/userCenter/activityList.vue
@@ -0,0 +1,3 @@
+<template>
+   揪團列表
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,43 @@ import forum from "../views/forum.vue"
 import activity from "../views/activity.vue"
 import login from "../views/login.vue"
 import room from "../views/room.vue"
+import usercenter from "../views/usercenter.vue"
+import userCenterSetting from "../components/userCenter/userCenterSetting.vue"
+import mailbox from "../components/userCenter/mailbox.vue"
+import activityList from "../components/userCenter/activityList.vue"
+import articleCollect from "../components/userCenter/articleCollect.vue"
 
 const router = new createRouter({
-    history: createWebHistory(),
-    routes: [
-      { path: "/", name: "index", component:index},
-      { path: "/forum", name: "forum", component: forum },
-      { path: "/activity", name: "activity", component: activity },
-      { path: "/App2", name: "App2", component: App2 },
-      { path: "/login", name: "login", component: login },
-      { path: "/room/:roomId", name: "room", component: room }
-    
-    ],
-  });
-  export default router;
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "index", component: index },
+    { path: "/forum", name: "forum", component: forum },
+    { path: "/activity", name: "activity", component: activity },
+    { path: "/App2", name: "App2", component: App2 },
+    { path: "/login", name: "login", component: login },
+    { path: "/room/:roomId", name: "room", component: room },
+    {
+      path: "/usercenter", name: "usercenter", component: usercenter, children: [
+        {
+          path: '/setting',
+          component: userCenterSetting
+        },
+        {
+          path: '/mailbox',
+          component: mailbox
+        },
+        {
+          path: '/activityList',
+          component: activityList
+        },
+        {
+          path: '/articleCollect',
+          component: articleCollect
+        },
+
+      ]
+    }
+
+  ],
+});
+export default router;
